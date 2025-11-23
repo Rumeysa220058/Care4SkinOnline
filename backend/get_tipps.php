a8d8b3817bb26d0bcdd0480d1bba703536c91d7e
Erina Iljazi <ilj220027@spengergasse.at>
2025-11-23 16:24:01 +0100

    Datenbankverbindung und Ergebnisbutton funktioniert

--- backend/get_tipps.php
+++ backend/get_tipps.php
@@ -1,6 +1,13 @@
 <?php
+// CORS HEADER - GANZ OBEN!
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=utf-8");
+
+// Dein bestehender Code...
 $pdo = new PDO("mysql:host=localhost;dbname=care4skin;charset=utf8", "root", "");
 $stmt = $pdo->query("SELECT * FROM tipps_hauttyp");
 $tipps = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($tipps); // JSON zurückgeben
+echo json_encode($tipps);
 ?>
