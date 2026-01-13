1c16da6cc081095c26a75a9a941307fb97abb3e6
Erina Iljazi <ilj220027@spengergasse.at>
2026-01-13 01:12:25 +0100

    Fix Projekt for Node.js, db.php

--- api/db.php
+++ api/db.php
@@ -1,16 +1,18 @@
 <?php
 
-$host = "localhost";   // MySQL Server (lokal)
-$db   = "care4skin";   // Datenbankname
-$user = "root";        // Standard XAMPP Benutzer
-$pass = "";            // Standard XAMPP Passwort (leer)
+$host = getenv("MYSQLHOST");        // Railway Variable für Host
+$port = getenv("MYSQLPORT");        // Railway Variable für Port
+$db   = getenv("MYSQLDATABASE");    // Railway Variable für Datenbank
+$user = getenv("MYSQLUSER");        // Railway Variable für Username
+$pass = getenv("MYSQLPASSWORD");    // Railway Variable für Passwort
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8",
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
         $user,
         $pass
     );
+    echo "DB Verbindung erfolgreich!";
 } catch (PDOException $e) {
-    die("Datenbankverbindung fehlgeschlagen");
+    die("Datenbankverbindung fehlgeschlagen: " . $e->getMessage());
 }
