f919fde11c05f7c9e1adf083f1ca4f35436f0437
Hümeyra <EMI220014@spengergasse.at>
2026-01-14 19:46:50 +0100

    db.php wieder geändert

--- api/db.php
+++ api/db.php
@@ -1,37 +1,14 @@
-<?php
-
-$host = getenv("MYSQLHOST");        // Variable für Host
-$port = getenv("MYSQLPORT");        // Variable für Port
-$db   = getenv("MYSQLDATABASE");    // Variable für Datenbank
-$user = getenv("MYSQLUSER");        // Variable für Username
-$pass = getenv("MYSQLPASSWORD");    // Variable für Passwort
+<?php $host = "localhost"; // MySQL Server (lokal) 
+$db = "care4skin"; // Datenbankname 
+$user = "root"; // Standard XAMPP Benutzer 
+$pass = ""; // Standard XAMPP Passwort (leer) 
 
 try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
-        $user,
-        $pass
-    );
-    // echo nur lokal zum Testen
-} catch (PDOException $e) {
-    die("Datenbankverbindung fehlgeschlagen: " . $e->getMessage());
-}
-
-//diesen Teil beim Hosting verwendet
-
-//<?php
-
-//$host = "mysql80.local:3307";   // MySQL Server (lokal)
-//$db   = "u242260db2";          // Datenbankname
-//$user = "u242260db2";         // Standard XAMPP Benutzer
-//$pass = "123456";            // Standard XAMPP Passwort (leer)
-
-//try {
-//    $pdo = new PDO(
-//        "mysql:host=$host;dbname=$db;charset=utf8",
-//        $user,
-//        $pass
-//    );
-//} catch (PDOException $e) {
-//    die("Datenbankverbindung fehlgeschlagen");
-//}
+     $pdo = new PDO(
+         "mysql:host=$host;dbname=$db;charset=utf8",
+          $user,
+           $pass
+            ); 
+    } catch (PDOException $e) {
+         die("Datenbankverbindung fehlgeschlagen");
+     }
