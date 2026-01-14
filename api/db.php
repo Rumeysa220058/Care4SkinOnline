8f52c8a608906e565eaab9ff34dcbf967f6249cd
Hümeyra <EMI220014@spengergasse.at>
2026-01-14 19:55:44 +0100

    versuch 3

--- api/db.php
+++ api/db.php
@@ -1,11 +1,7 @@
-<?php
-
-$host = getenv("MYSQLHOST");        // Railway Variable für Host
-$port = getenv("MYSQLPORT");        // Railway Variable für Port
-$db   = getenv("MYSQLDATABASE");    // Railway Variable für Datenbank
-$user = getenv("MYSQLUSER");        // Railway Variable für Username
-$pass = getenv("MYSQLPASSWORD");    // Railway Variable für Passwort
-
+<?php $host = "localhost"; // MySQL Server (lokal) 
+$db = "care4skin"; // Datenbankname 
+$user = "root"; // Standard XAMPP Benutzer 
+$pass = ""; // Standard XAMPP Passwort (leer) 
 try {
     $pdo = new PDO(
         "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
