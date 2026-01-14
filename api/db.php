b32c8b4fd15cef8769ab22fe4cbe42ed90c5b06f
Hümeyra <EMI220014@spengergasse.at>
2026-01-14 20:01:19 +0100

    engültiger version

--- api/db.php
+++ api/db.php
@@ -1,10 +1,11 @@
-<?php $host = "localhost"; // MySQL Server (lokal) 
-$db = "care4skin"; // Datenbankname 
-$user = "root"; // Standard XAMPP Benutzer 
-$pass = ""; // Standard XAMPP Passwort (leer) 
+<?php
+$host = "localhost"; // MySQL Server (lokal)
+$db   = "care4skin"; // Datenbankname
+$user = "root";       // Standard XAMPP Benutzer
+$pass = "";           // Standard XAMPP Passwort (leer)
 try {
     $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
+        "mysql:host=$host;dbname=$db;charset=utf8",
         $user,
         $pass
     );
